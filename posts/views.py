1e0c7fb1b63d8d67af4684e5190a111cd218ba79
loak155 <loak155.0620@gmail.com>
2020-08-16 21:14:29 +0900

    テンプレートファイルの追加

--- posts/views.py
+++ posts/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello,world!")
-
+    # return HttpResponse("Hello,world!")
+    return render(request, 'posts/index.html')
 
 # Create your views here.
